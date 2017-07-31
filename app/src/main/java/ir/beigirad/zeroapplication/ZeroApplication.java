e67baad2dd02e230767d4a4400a1320a1e592af1
farhad-mbp <f.beigirad@gmail.com>
2017-07-31 17:32:07 +0430

    Added Application

---
+++ app/src/main/java/ir/beigirad/zeroapplication/ZeroApplication.java
@@ -0,0 +1,14 @@
+package ir.beigirad.zeroapplication;
+
+import android.app.Application;
+
+/**
+ * Created by farhad-mbp on 7/31/17.
+ */
+
+public class ZeroApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
